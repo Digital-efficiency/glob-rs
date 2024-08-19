6509f4a9fda767d5e7a3c0c64e41b3fbc3efd061
newstart0514 <1418591636@qq.com>
2024-08-19 16:04:05 +0800

    add tags & add test

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,13 @@
-// import test from 'ava'
+import test from 'ava'
 
-// import { sum } from '../index.js'
+import { findMatchingFiles, readFileContent } from '../index.js'
 
-// test('sum from native', (t) => {
-//   t.is(sum(1, 2), 3)
-// })
+test('find_matching_files function pass', (t) => {
+  const result = findMatchingFiles(['*.js', 1]);
+  t.true(Array.isArray(result));
+})
+
+test('read_file_content function pass', (t) => {
+  const result = readFileContent('./test.js');
+  t.is(typeof result, 'string');
+})
